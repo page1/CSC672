393ce2a049892fdf9f5885a9a37da1c63fe0ae23
Scott Page <6609556+page1@users.noreply.github.com>
2018-01-15 19:00:39 -0700

    Revert "Initial data reading commit"
    This reverts commit e937dd0d8fa605ad11595ea8b1a1ed1706f81ec6.

--- README.md
+++ README.md
@@ -1,4 +1,1 @@
 # CSC672
-
-## Setup
-1. Place subfolders of "sample data" in the "data" folder. This folder's content is not checked into github due to size.
--- preliminary_analysis.R
+++
@@ -1,8 +0,0 @@
-if (!require("pacman")) install.packages("pacman")
-pacman::p_load("dplyr")
-
-subfolders <- list.dirs(path = "data", full.names = TRUE, recursive = TRUE)
-subfolders <- subfolders[-which(subfolders == 'data')]
-data_files <- paste(subfolders, 'masterFile.csv', sep = .Platform$file.sep)
-
-data <- lapply(data_files, read.csv)
