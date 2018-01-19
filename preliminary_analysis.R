41241b3544dbcae34eb8209a4e6061e5679d6ec9
Scott Page <6609556+page1@users.noreply.github.com>
2018-01-19 14:46:10 -0700

    Add time plots. Skip generation of files that exist.

--- preliminary_analysis.R
+++ preliminary_analysis.R
@@ -18,6 +18,7 @@ for(column_name in column_names){#column_names
   print(column_name)
   combined_columns <- lapply(names(data), function(dataset_name){
     data.frame('dataset' = dataset_name, 
+               'time' = data[[dataset_name]]$ElapsedTimeInVideo,
                'value' = data[[dataset_name]][[column_name]])
     
   }) %>% do.call('rbind', .) %>%
@@ -26,21 +27,38 @@ for(column_name in column_names){#column_names
       if(column_name %in% c('RectBigSide', 'RectRatio', 'SegStatus', 'Kim')){
         #do special things
       } else {
-        prelim_hist <- ggplot(data=combined_columns, 
-                              aes(x = value, colour = dataset)) + 
-          geom_density() +
-          ggtitle(paste(column_name)) +
-          ylab('Density') +
-          xlab('Value')
-        ggsave(file.path('prelim', column_name, paste("density_compare_", column_name, ".png", sep = "")), plot = prelim_hist)
+        file <- file.path('prelim', column_name, paste("density_compare_", column_name, ".png", sep = ""))
+        if(!file.exists(file)){
+          prelim_hist <- ggplot(data=combined_columns, 
+                                aes(x = value, colour = dataset)) + 
+            geom_density() +
+            ggtitle(paste(column_name)) +
+            ylab('Density') +
+            xlab('Value')
+          ggsave(file, plot = prelim_hist)
+        }
         
-        prelim_hist <- ggplot(data=combined_columns, 
-                              aes(x = value, y = ..scaled.., colour = dataset)) + 
-          geom_density() +
-          ggtitle(paste(column_name)) +
-          ylab('Scaled Density') +
-          xlab('Value')
-        ggsave(file.path('prelim', column_name, paste("scaled_density_compare_", column_name, ".png", sep = "")), plot = prelim_hist)
+        file <- file.path('prelim', column_name, paste("scaled_density_compare_", column_name, ".png", sep = ""))
+        if(!file.exists(file)){
+          prelim_hist <- ggplot(data=combined_columns, 
+                                aes(x = value, y = ..scaled.., colour = dataset)) + 
+            geom_density() +
+            ggtitle(paste(column_name)) +
+            ylab('Scaled Density') +
+            xlab('Value')
+          ggsave(file, plot = prelim_hist)
+        }
+        
+        file <- file.path('prelim', column_name, paste("timeplot_compare_", column_name, ".png", sep = ""))
+        if(!file.exists(file)){
+          prelim_timeplot <- ggplot(data=combined_columns, 
+                                aes(x = time, y = value, colour = dataset)) + 
+            geom_line() +
+            ggtitle(paste(column_name)) +
+            ylab('Value') +
+            xlab('Time')
+          ggsave(file, plot = prelim_timeplot)
+        }
       }
     },
     error = function(e) {print(paste('issue with', column_name, e))}
