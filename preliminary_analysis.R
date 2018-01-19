450664bb4d47b13b19c600115376565a728a160a
Scott Page <6609556+page1@users.noreply.github.com>
2018-01-19 11:56:23 -0700

    Initial comparative plots of density of individual variables. Some errors still exist.

--- preliminary_analysis.R
+++ preliminary_analysis.R
@@ -1,8 +1,34 @@
 if (!require("pacman")) install.packages("pacman")
-pacman::p_load("dplyr")
+pacman::p_load("dplyr",
+               "ggplot2")
 
 subfolders <- list.dirs(path = "data", full.names = TRUE, recursive = TRUE)
 subfolders <- subfolders[-which(subfolders == 'data')]
 data_files <- paste(subfolders, 'masterFile.csv', sep = .Platform$file.sep)
+folders <- gsub('data/', '', subfolders, fixed = T)
 
 data <- lapply(data_files, read.csv)
+names(data) <- folders
+
+column_names <- unique(unlist(lapply(data, names)))
+
+# Gives errors on: RectBigSide, RectRatio, SegStatus, Kim
+for(column_name in column_names){
+  dir.create(file.path('prelim', column_name), recursive = T, showWarnings = F)
+  print(column_name)
+  combined_columns <- lapply(names(data), function(dataset_name){
+    data.frame('dataset' = dataset_name, 
+               'value' = data[[dataset_name]][[column_name]])
+    
+  }) %>% do.call('rbind', .) %>%
+    filter(!is.null(value))
+  tryCatch({
+      prelim_hist <- ggplot(data=combined_columns, 
+                            aes(x = value, y = ..density.., colour = dataset)) + 
+        geom_freqpoly() +
+        ggtitle(paste(column_name))
+      ggsave(file.path('prelim', column_name, paste("density_compare_", column_name, ".png", sep = "")), plot = prelim_hist)
+    },
+    error = function(e) {print(paste('issue with', column_name, e))}
+  )
+}
